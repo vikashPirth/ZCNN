628f1eb8fa965b2d6acba7b7816d00b0e8a177a3
vikashPirth <vikash.pirthiani@gmail.com>
2024-01-11 21:30:36 +0500

    Mnist load data

---
+++ main.py
@@ -0,0 +1,10 @@
+import tensorflow as tf 
+import matplotlib.pyplot as plt
+
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+plt.imshow(x_train[0], cmap="gray")
